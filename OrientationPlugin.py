27c5b37a5528498902ebafd6b4fa7c1f154eaf14
5axes <5axes@users.noreply.github.com>
2022-07-12 13:52:12 +0200

    Add Function Rotate main direction (X)
    add Function Rotate main direction (X)
    Orient the longest border of the Part on the X Direction

--- OrientationPlugin.py
+++ OrientationPlugin.py
@@ -4,18 +4,27 @@ from UM.Extension import Extension
 from UM.PluginRegistry import PluginRegistry
 from UM.Scene.SceneNode import SceneNode
 from UM.Scene.Selection import Selection
+from UM.Scene.SceneNode import SceneNode
+from UM.Operations.GroupedOperation import GroupedOperation
+
+
+from UM.Math.Vector import Vector
+from UM.Math.Matrix import Matrix
 
 from UM.Message import Message
+from UM.Logger import Logger
 from cura.CuraApplication import CuraApplication
 
 from cura.CuraVersion import CuraVersion  # type: ignore
 from UM.Version import Version
 
 from .CalculateOrientationJob import CalculateOrientationJob
+from .SetTransformMatrixOperation import SetTransformMatrixOperation
 
 from UM.i18n import i18nCatalog
 
 import os
+import numpy
 i18n_catalog = i18nCatalog("OrientationPlugin")
 
 
@@ -25,13 +34,17 @@ class OrientationPlugin(Extension):
         self.addMenuItem(i18n_catalog.i18n("Calculate fast optimal printing orientation"), self.doFastAutoOrientation)
         self.addMenuItem(i18n_catalog.i18n("Calculate extended optimal printing orientation"), self.doExtendedAutoOrientiation)
         self.addMenuItem("", lambda: None)
+        self.addMenuItem(i18n_catalog.i18nc("@item:inmenu", "Rotate main direction (X)"), self.rotateMainDirection)
+        self.addMenuItem(" ", lambda: None)
         self.addMenuItem(i18n_catalog.i18n("Modify Settings"), self.showPopup)
-        self._message = None
-
+        self._message = Message(title=i18n_catalog.i18nc("@info:title", "Orientation Plugin"))
+        
         self._currently_loading_files = []  # type: List[str]
         self._check_node_queue = []  # type: List[SceneNode]
         CuraApplication.getInstance().getPreferences().addPreference("OrientationPlugin/do_auto_orientation", False)
         self._do_auto_orientation = CuraApplication.getInstance().getPreferences().getValue("OrientationPlugin/do_auto_orientation")
+        
+        
         # Should the volume beneath the overhangs be penalized?
         CuraApplication.getInstance().getPreferences().addPreference("OrientationPlugin/min_volume", True)
 
@@ -105,6 +118,129 @@ class OrientationPlugin(Extension):
 
         self._check_node_queue = []
 
+    def _getSelectedNodes(self, force_single = False) -> List[SceneNode]:
+        self._message.hide()
+        selection = Selection.getAllSelectedObjects()[:]
+        if force_single:
+            if len(selection) == 1:
+                return selection[:]
+
+            self._message.setText(i18n_catalog.i18nc("@info:status", "No object selected to orient. Please select one object and try again."))
+        else:
+            if len(selection) >= 1:
+                return selection[:]
+
+            self._message.setText(i18n_catalog.i18nc("@info:status", "No objects selected to orient. Please select one or more objects and try again."))
+
+        self._message.show()
+        return []
+        
+    def rotateMainDirection(self) -> None:
+        nodes_list = self._getSelectedNodes()
+        if not nodes_list:
+            return
+
+        op = GroupedOperation()
+        for node in nodes_list:
+            mesh_data = node.getMeshData()
+            if not mesh_data:
+                continue
+            
+            hull_polygon = node.callDecoration("_compute2DConvexHull")
+            # test but not sure in witch case we have this situation ?
+            if not hull_polygon or hull_polygon.getPoints is None:
+                Logger.log("w", "Object {} cannot be calculated because it has no convex hull.".format(node.getName()))
+                continue
+ 
+            points=hull_polygon.getPoints()
+
+            # Init
+            np = 0
+            l_v = 0
+            for point in points:
+                # Logger.log('d', "p{} X : {} Y : {}".format(np,point[0],point[1]))
+                if np>0 :                         
+                    new_position = Vector(point[0], point[1], 0)
+                    lg=new_position-first_pt
+                    lght = lg.length()
+                    if lght>l_v :
+                        l_v = lght
+                        s_lg=lg
+
+                    first_pt=new_position
+                else :
+                    first_pt = Vector(point[0], point[1], 0)             
+                np+=1
+            
+            # Last vector  to close the loop       
+            lg=Vector(points[0][0], points[0][1], 0)-first_pt
+            lght = lg.length()
+            if lght>l_v :
+                s_lg=lg
+                # Logger.log('d', "s_lg on las point : {}".format(s_lg))
+            
+            vectX = (1, 0, 0)
+            vectY = (0, 1, 0)
+            anGl= self._angle_between(vectX,(s_lg.x, s_lg.y, 0))
+            deganGl = anGl/numpy.pi*180
+            
+            # For debuging output the vector director and the Angle ( in radians and degree)
+            Logger.log('d', "s_lg   : {}".format(s_lg))
+            Logger.log('d', "Angle : {} Angle° : {}".format(anGl,deganGl)) 
+            #Something done in the RotateTool.py ... It's not very clear for me, but I still have to investigate this point
+            dv=self._dot_vector(vectY,(s_lg.x, s_lg.y, 0))
+            Logger.log('d', "Dot vector : {}".format(dv))  
+            if dv > 0 :
+                direction = 1
+            else :
+                direction = -1
+                
+            extents = mesh_data.getExtents()
+            center = Vector(extents.center.x, extents.center.y, extents.center.z)
+
+            # Get the Rotation Matrix
+            rotation = Matrix()
+            # setByRotationAxis or rotateByAxis ?  don't think there is a difference
+            rotation.setByRotationAxis(direction*anGl, Vector(0, 1, 0))
+            Logger.log('d', "Rotation                 :\n{}".format(rotation))
+            
+            # Change Transfo data
+            local_transformation = Matrix()      
+            local_transformation.multiply(rotation)
+            local_transformation.multiply(node.getLocalTransformation()) 
+            # Log for debugging and Analyse           
+            Logger.log('d', "Local_transformation    :\n{}".format(node.getLocalTransformation())) 
+            Logger.log('d', "TransformMatrixOperation :\n{}".format(local_transformation))                       
+            op.addOperation(SetTransformMatrixOperation(node, local_transformation))
+
+        op.push()
+    
+    def _unit_vector(self, vector):
+        """ Returns the unit vector of the vector.  """
+        return vector / numpy.linalg.norm(vector)
+
+    def _angle_between(self, v1, v2):
+        """ Returns the angle in radians between vectors 'v1' and 'v2'::
+        """
+
+        v1_u = self._unit_vector(v1)
+        v2_u = self._unit_vector(v2)
+        
+        angle = numpy.arccos(numpy.clip(numpy.dot(v1_u, v2_u), -1.0, 1.0))
+     
+        return angle
+ 
+    def _dot_vector(self, v1, v2):
+        """ Returns the scalar product between vectors 'v1' and 'v2'::
+        """
+
+        v1_u = self._unit_vector(v1)
+        v2_u = self._unit_vector(v2)
+        
+        dotv = numpy.dot(v1_u, v2_u)
+     
+        return dotv
+        
     def doFastAutoOrientation(self):
         self.doAutoOrientation(False)
 
